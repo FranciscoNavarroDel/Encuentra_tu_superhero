77247540b509668d9978455331bbf6fb807f48ac
Francisco <f.nav.del@gmail.com>
2021-05-29 19:50:16 -0400

    ultimos arreglos y comentarios del código

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,24 @@
+// jQuery.fn.extraerinfo es llamada desde submit del formulario
+
 jQuery.fn.extraerinfo = function() {
-    var element = this;
+    // Input usuario es lo que se extra desde el formulario
+    let inputusuario = this;
+
+    // conectamos a la api mediante ajax
     $.ajax({
         type: "GET",
-        url: "https://superheroapi.com/api.php/10159431839884446/" + element.val(),
+        url: "https://superheroapi.com/api.php/10159431839884446/" + inputusuario.val(),
         dataType: "json",
         success: function(data) {
+            // extraemos la respuesta que nos entrega la api en el response y un posible error por la id solicitada
             let { response, error } = data
 
 
-
+            // Se realiza un if succes para ejecutar la extraccion de los datos si es que fue exitosa la respuesta de la api
+            // y else en caso de que el valor devuelto por la id requerida sea error se realiza un alert indicando que el valor de la id no es valido
             if (response == "success") {
 
+                // se descomprime la informacion requerida para el super heroe
                 let {
                     powerstats,
                     work: { occupation: ocupacion },
@@ -23,6 +31,8 @@ jQuery.fn.extraerinfo = function() {
                 let { combat, durability, intelligence, power, speed, strength } = powerstats
 
                 let status = [combat, durability, intelligence, power, speed, strength]
+
+                // se transpasa la informacion al doom 
                 $(".encontrado").text("SuperHero Encontrado")
                 $(".imagendata").attr(`src`, `${image.url}`);
                 $(".nombre").text(`Nombre: ${name}`);
@@ -34,14 +44,18 @@ jQuery.fn.extraerinfo = function() {
                 $(".peso").text(`Peso: ${peso[0]}, ${peso[1]}`);
                 $(".biografia").text(`Alter-ego: ${alterego}`);
 
+                // usamos un metodo map para pasar de un array de aliados a un string con los aliados del heroe
                 let aliados = ""
                 aliados += aliases.map((aliado) => aliado);
                 $(".alianzas").text(`Alianzas: ${aliados}`);
+                console.log(aliados);
 
+                // utilizamos un metodo para modificar los valores null a 0 para que no produzca un error al ejecutar canvas
                 status.map((element, index) => {
                     if (element == "null") status[index] = 0
                 })
 
+                // se utiliza canvas y se setean los valores y propiedades del gráfico
                 var chart = new CanvasJS.Chart("chartContainer", {
                     theme: "light1",
 
@@ -82,11 +96,13 @@ jQuery.fn.extraerinfo = function() {
                 alert(error)
             }
         },
+        // se alerta del error en caso de que la api no responda
         error: function(error) {
             alert(error)
         }
     });
 };
+
 
 $(".imagen_desplegable").hide();
 $(".superheroinfo").hide()
@@ -95,16 +111,19 @@ $(document).ready(function() {
     $("form").submit(function(event) {
         event.preventDefault()
         if (Number($("#superheronumber").val())) {
+            // al obtener un numero se llama a la funcion extraerinfo que muestra al super heroe recibido desde la api
             $("#superheronumber").extraerinfo();
             $("h4").text("Ingresa el número del SuperHero a buscar").css("color", "black")
             $(".imagen_desplegable").slideDown(3000);
             $(".superheroinfo").show()
         } else if (Number($("#superheronumber").val()) == 0) {
+            // en caso de ingresar el valor 0 se indica en el doom que el valor debe ser mayor a 0
             $("h4").text("El valor ingresado debe ser mayor a 0").css("color", "red");
             $(".imagen_desplegable").hide(1000)
             $(".encontrado").text("")
             $(".superheroinfo").hide(1000)
         } else {
+            // se realiza la alerta en caso de que el valor no sea un número
             $("h4").text("El valor ingresado no es un número").css("color", "red");
             $(".imagen_desplegable").hide(1000)
             $(".encontrado").text("")

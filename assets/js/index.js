2099b592e0f354adb70661036aa9580b981ee0d4
Francisco <f.nav.del@gmail.com>
2021-05-29 20:28:09 -0400

    se elimna los console log del código

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,7 @@
 // jQuery.fn.extraerinfo es llamada desde submit del formulario
-
 jQuery.fn.extraerinfo = function() {
     // Input usuario es lo que se extra desde el formulario
     let inputusuario = this;
-
     // conectamos a la api mediante ajax
     $.ajax({
         type: "GET",
@@ -12,12 +10,9 @@ jQuery.fn.extraerinfo = function() {
         success: function(data) {
             // extraemos la respuesta que nos entrega la api en el response y un posible error por la id solicitada
             let { response, error } = data
-
-
             // Se realiza un if succes para ejecutar la extraccion de los datos si es que fue exitosa la respuesta de la api
             // y else en caso de que el valor devuelto por la id requerida sea error se realiza un alert indicando que el valor de la id no es valido
             if (response == "success") {
-
                 // se descomprime la informacion requerida para el super heroe
                 let {
                     powerstats,
@@ -29,10 +24,8 @@ jQuery.fn.extraerinfo = function() {
                 let { biography: { publisher: publicado, "first-appearance": aparicion, aliases: aliases } } = data
                 let { appearance: { height: altura, weight: peso } } = data
                 let { combat, durability, intelligence, power, speed, strength } = powerstats
-
                 let status = [combat, durability, intelligence, power, speed, strength]
-
-                // se transpasa la informacion al doom 
+                    // se transpasa la informacion al doom 
                 $(".encontrado").text("SuperHero Encontrado")
                 $(".imagendata").attr(`src`, `${image.url}`);
                 $(".nombre").text(`Nombre: ${name}`);
@@ -42,23 +35,17 @@ jQuery.fn.extraerinfo = function() {
                 $(".primera_aparicion").text(`Primera aparicion: ${aparicion}`)
                 $(".altura").text(`Altura: ${altura[0]}, ;${altura[1]}`)
                 $(".peso").text(`Peso: ${peso[0]}, ${peso[1]}`);
-
-
                 // usamos un metodo map para pasar de un array de aliados a un string con los aliados del heroe
                 let aliados = ""
                 aliados += aliases.map((aliado) => aliado);
                 $(".alianzas").text(`Alianzas: ${aliados}`);
-                console.log(aliados);
-
                 // utilizamos un metodo para modificar los valores null a 0 para que no produzca un error al ejecutar canvas
                 status.map((element, index) => {
-                    if (element == "null") status[index] = 0
-                })
-
-                // se utiliza canvas y se setean los valores y propiedades del gráfico
+                        if (element == "null") status[index] = 0
+                    })
+                    // se utiliza canvas y se setean los valores y propiedades del gráfico
                 var chart = new CanvasJS.Chart("chartContainer", {
                     theme: "light1",
-
                     animationEnabled: true,
                     title: {
                         text: `Estadísticas de Poder para ${name}`,
@@ -102,7 +89,6 @@ jQuery.fn.extraerinfo = function() {
         }
     });
 };
-
 
 $(".imagen_desplegable").hide();
 $(".superheroinfo").hide()

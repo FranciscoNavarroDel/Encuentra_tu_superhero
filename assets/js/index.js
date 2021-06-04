eb380d51a45567c6bc6a9de6abf7223ee691047b
Francisco <f.nav.del@gmail.com>
2021-06-04 16:06:49 -0400

    final

--- assets/js/index.js
+++ assets/js/index.js
@@ -44,7 +44,6 @@ jQuery.fn.extraerinfo = function() {
                     data_stats.push({ y: stat[1], label: stat[0] })
                 })
 
-
                 // se utiliza canvas y se setean los valores y propiedades del gráfico
                 var chart = new CanvasJS.Chart("chartContainer", {
                     theme: "light1",
